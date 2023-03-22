fd2a80c90a51efdac4696678efd0c5eefc518a3e
RESIDGE <k.boc@yahoo.com>
2023-03-22 14:25:32 -0700

    Created frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import { ReactDOM } from "react";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
