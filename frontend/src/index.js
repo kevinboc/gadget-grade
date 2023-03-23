d6f8599ddcc5b8032ea34d9d6de69d82567a1beb
RESIDGE <k.boc@yahoo.com>
2023-03-22 18:23:02 -0700

    Fixed App.js to React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-import { ReactDOM } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 
 root.render(<App />);
