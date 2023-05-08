54aa5be16762a253213997dcf4e55cc86c364d4b
RESIDGE <k.boc@yahoo.com>
2023-05-07 22:45:58 -0700

    Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./layouts/Header.jsx"
 import AppRoutes from "./routes/AppRoutes.jsx"
+import Footer from "./layouts/Footer.jsx"
 
 const App = () => {
   return (
     <div>
       <Header />
       <AppRoutes />
+      <Footer />
     </div>
   )
 }
