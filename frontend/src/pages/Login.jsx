8570d783fffe6785b31f7b7698d5f3c5b52ac757
Cameron <camharris831@gmail.com>
2023-05-13 13:13:55 -0700

    static finished needs backend connection

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,34 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import React, { useState } from 'react';
 
-export default Login
+const LoginPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isLogin) {
+      // Perform login
+      console.log("Logging in:", { username, password });
+    } else {
+      // Perform signup
+      console.log("Signing up:", { username, password });
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: '#f0f0f0' }}>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px', padding: '20px', background: '#fff', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)', borderRadius: '10px' }}>
+        <h2>{isLogin ? 'Login' : 'Sign Up'}</h2>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ margin: '10px 0', padding: '10px', fontSize: '16px' }} />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ margin: '10px 0', padding: '10px', fontSize: '16px' }} />
+        <button type="submit" style={{ background: '#007BFF', color: '#fff', border: 'none', borderRadius: '5px', padding: '10px', cursor: 'pointer', fontSize: '18px' }}>{isLogin ? 'Login' : 'Sign Up'}</button>
+        <button type="button" onClick={() => setIsLogin(!isLogin)} style={{ background: 'transparent', border: 'none', cursor: 'pointer', textDecoration: 'underline', marginTop: '10px', color: '#007BFF' }}>
+          {isLogin ? 'Need to create an account?' : 'Already have an account?'}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
